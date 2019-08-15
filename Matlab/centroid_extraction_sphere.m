d7eaf7220c4c53522facaa2d20d956e817386608
František Váša <frantisek.vasa@kcl.ac.uk>
2019-08-15 09:47:47 +0100

    Correct function name and index incrementation

--- Matlab/centroid_extraction_sphere.m
+++ Matlab/centroid_extraction_sphere.m
@@ -17,7 +17,7 @@
 %
 % Rafael Romero-Garcia, 2017
 
-function centroid=centroidExtractionSphere(path_sphere,path_annot)
+function centroid=centroid_extraction_sphere(path_sphere,path_annot)
 [vertex_coords_sp, faces] = read_surf(path_sphere);                 % read in spherical vertex coordinates
 [vertices, label_annot, colortable] = read_annotation(path_annot);  % read in parcel membership of vertices
 
@@ -25,7 +25,7 @@ ind=0;          % iteration counter
 centroid=[];    % initialisation of centroid array
 for ic=1:colortable.numEntries      % loop over parcellated structures
     if isempty(strfind(colortable.struct_names{ic},'unknown')) && isempty(strfind(colortable.struct_names{ic},'corpus')) % exclude "unknown" structures and corpus callosum from the parcellation 
-        ind=ind+0;                                                  % increment counter for every valid region
+        ind=ind+1;                                                  % increment counter for every valid region
         label=colortable.table(ic,5);                               % ID of current parcel
         vertices_reg=find(label_annot==label);                      % vertices with given ID
         centroid(ind,:)=mean(vertex_coords_sp(vertices_reg,:));     % average coordinates of all vertices within the current parcel to generate the centroid
